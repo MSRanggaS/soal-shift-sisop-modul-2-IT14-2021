eecd7294ae6be6f5d9dd2238ac14a70094475ecd
MSRanggaS <61416036+MSRanggaS@users.noreply.github.com>
2021-04-17 12:24:22 +0700

    Update soal1.c

--- soal 1/soal1.c
+++ soal 1/soal1.c
@@ -46,101 +46,104 @@ int main(){
 
         w = time(NULL);
         wm = localtime(&w);
-        strftime(file, 50, "%Y-%m-%d_%H:%M:%S", wm);
+        strftime(file, 50, "%Y-%m-%d %H:%M:%S", wm);
         if(strcmp(file,"2021-04-09 16:22:00") == 0){
 
             if (child_id == 0) {
                 char *argv[] = {"mkdir", "-p", "Musyik", NULL};
                 execv("/bin/mkdir", argv);
             }
-            while(wait(NULL) != child_id);
+            else 
+            {
 
-            child_id = fork();
-            if (child_id == 0) {
-                char *argv[] = {"mkdir", "-p", "Fylm", NULL};
-                execv("/bin/mkdir", argv);
-            }
-            while(wait(NULL) != child_id);
+                child_id = fork();
+                if (child_id == 0) {
+                    char *argv[] = {"mkdir", "-p", "Fylm", NULL};
+                    execv("/bin/mkdir", argv);
+                }
+                while(wait(NULL) != child_id);
 
-            child_id = fork();
-            if (child_id == 0) {
-                char *argv[] = {"mkdir", "-p", "Pyoto", NULL};
-                execv("/bin/mkdir", argv);
-            }
-            while(wait(NULL) != child_id);
+                child_id = fork();
+                if (child_id == 0) {
+                    char *argv[] = {"mkdir", "-p", "Pyoto", NULL};
+                    execv("/bin/mkdir", argv);
+                }
+                while(wait(NULL) != child_id);
 
-            child_id = fork();
-            if (child_id == 0) {
-                char *args[] = {"wget","--no-check-certificate", "-q", 
-                "https://drive.google.com/uc?id=1FsrAzb9B5ixooGUs0dGiBr-rC7TS9wTD&export=download",
-                "-O", "Foto_for_Stevany.zip", NULL};
-                execv("/usr/bin/wget", args);
-            }
-            while(wait(NULL) != child_id);
+                child_id = fork();
+                if (child_id == 0) {
+                    char *args[] = {"wget","--no-check-certificate", "-q", 
+                    "https://drive.google.com/uc?id=1FsrAzb9B5ixooGUs0dGiBr-rC7TS9wTD&export=download",
+                    "-O", "Foto_for_Stevany.zip", NULL};
+                    execv("/usr/bin/wget", args);
+                }
+                while(wait(NULL) != child_id);
 
-            child_id = fork();
-            if (child_id == 0) {
-                char *args[] = {"wget","--no-check-certificate", "-q", 
-                "https://drive.google.com/uc?id=1ZG8nRBRPquhYXq_sISdsVcXx5VdEgi-J&export=download",
-                "-O", "Musik_for_Stevany.zip", NULL};
-                execv("/usr/bin/wget", args);
-            }
-            while(wait(NULL) != child_id);
+                child_id = fork();
+                if (child_id == 0) {
+                    char *args[] = {"wget","--no-check-certificate", "-q", 
+                    "https://drive.google.com/uc?id=1ZG8nRBRPquhYXq_sISdsVcXx5VdEgi-J&export=download",
+                    "-O", "Musik_for_Stevany.zip", NULL};
+                    execv("/usr/bin/wget", args);
+                }
+                while(wait(NULL) != child_id);
 
 
-            child_id = fork();
-            if (child_id == 0) {
-                char *args[] = {"wget","--no-check-certificate", "-q", 
-                "https://drive.google.com/uc?id=1ktjGgDkL0nNpY-vT7rT7O6ZI47Ke9xcp&export=download",
-                "-O", "Film_for_Stevany.zip", NULL};
-                execv("/usr/bin/wget", args);
-            }
-            while(wait(NULL) != child_id);
+                child_id = fork();
+                if (child_id == 0) {
+                    char *args[] = {"wget","--no-check-certificate", "-q", 
+                    "https://drive.google.com/uc?id=1ktjGgDkL0nNpY-vT7rT7O6ZI47Ke9xcp&export=download",
+                    "-O", "Film_for_Stevany.zip", NULL};
+                    execv("/usr/bin/wget", args);
+                }
+                while(wait(NULL) != child_id);
 
-            child_id = fork();
-            if (child_id == 0) {
-                char *argv[] = {"unzip", "-q", "Foto_for_Stevany.zip", NULL};
-                execv("/usr/bin/unzip", argv);
-            }
-            while(wait(NULL) != child_id);
+                child_id = fork();
+                if (child_id == 0) {
+                    char *argv[] = {"unzip", "-q", "Foto_for_Stevany.zip", NULL};
+                    execv("/usr/bin/unzip", argv);
+                }
+                while(wait(NULL) != child_id);
 
-            child_id = fork();
-            if (child_id == 0) {
-                char *argv[] = {"unzip", "-q", "Musik_for_Stevany.zip", NULL};
-                execv("/usr/bin/unzip", argv);
-            }
-            while(wait(NULL) != child_id);
+                child_id = fork();
+                if (child_id == 0) {
+                    char *argv[] = {"unzip", "-q", "Musik_for_Stevany.zip", NULL};
+                    execv("/usr/bin/unzip", argv);
+                }
+                while(wait(NULL) != child_id);
 
-            child_id = fork();
-            if (child_id == 0) {
-                char *argv[] = {"unzip", "-q", "Film_for_Stevany.zip", NULL};
-                execv("/usr/bin/unzip", argv);
-            }
-            while(wait(NULL) != child_id);
+                child_id = fork();
+                if (child_id == 0) {
+                    char *argv[] = {"unzip", "-q", "Film_for_Stevany.zip", NULL};
+                    execv("/usr/bin/unzip", argv);
+                }
+                while(wait(NULL) != child_id);
 
-            child_id = fork();
-            if (child_id == 0) {
-            char *argv[] = {"find","FOTO","-type","f","-name","*",
-                            "-exec","mv","-t","Pyoto/","{}","+",NULL};
-            execv("/bin/find", argv);
-            }
-            while(wait(NULL) != child_id);
+                child_id = fork();
+                if (child_id == 0) {
+                char *argv[] = {"find","FOTO","-type","f","-name","*",
+                                "-exec","mv","-t","Pyoto/","{}","+",NULL};
+                execv("/bin/find", argv);
+                }
+                while(wait(NULL) != child_id);
 
-            child_id = fork();
-            if (child_id == 0) {
-            char *argv[] = {"find","MUSIK","-type","f","-name","*",
-                            "-exec","mv","-t","Musyik/","{}","+",NULL};
-            execv("/bin/find", argv);
-            }
-            while(wait(NULL) != child_id);
+                child_id = fork();
+                if (child_id == 0) {
+                char *argv[] = {"find","MUSIK","-type","f","-name","*",
+                                "-exec","mv","-t","Musyik/","{}","+",NULL};
+                execv("/bin/find", argv);
+                }
+                while(wait(NULL) != child_id);
 
-            child_id = fork();
-            if (child_id == 0) {
-            char *argv[] = {"find","FILM","-type","f","-name","*",
-                            "-exec","mv","-t","Fylm/","{}","+",NULL};
-            execv("/bin/find", argv);
-            }
-            while(wait(NULL) != child_id);
+                child_id = fork();
+                if (child_id == 0) {
+                char *argv[] = {"find","FILM","-type","f","-name","*",
+                                "-exec","mv","-t","Fylm/","{}","+",NULL};
+                execv("/bin/find", argv);
+                }
+                while(wait(NULL) != child_id);
+
+            }            
 
             break;
 
@@ -152,7 +155,7 @@ int main(){
 
         w = time(NULL);
         wm = localtime(&w);
-        strftime(file, 50, "%Y-%m-%d_%H:%M:%S", wm);
+        strftime(file, 50, "%Y-%m-%d %H:%M:%S", wm);
         if(strcmp(file,"2021-04-09 22:22:00") == 0){
 
             child_id = fork();
@@ -160,14 +163,16 @@ int main(){
                 char *argv[] = {"zip", "-r", "Lopyu_Stevany.zip", "Pyoto", "Musyik", "Fylm", NULL};
                 execv("/usr/bin/zip", argv);
             }
-            while(wait(NULL) != child_id);
+            else{
 
-            child_id = fork();
-            if (child_id == 0) {
-                char *argv[] = {"rm", "-r", "FILM", "FOTO", "MUSIK", "Pyoto", "Fylm", "Musyik", NULL};
-                execv("/bin/rm", argv);
+                child_id = fork();
+                if (child_id == 0) {
+                    char *argv[] = {"rm", "-r", "FILM", "FOTO", "MUSIK", "Pyoto", "Fylm", "Musyik", NULL};
+                    execv("/bin/rm", argv);
+                }
+                while(wait(NULL) != child_id);
+
             }
-            while(wait(NULL) != child_id);
 
             break;
 
